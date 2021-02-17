ec7d105b5d79e07e6466fe26482f4ef61b2f433b
Roman911 <roma-lysyk@ukr.net>
2021-02-17 18:55:05 +0200

    Deploy

--- modules/Likes/Components/LikeIcon.tsx
+++ modules/Likes/Components/LikeIcon.tsx
@@ -1,13 +1,6 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { IconProp } from "@fortawesome/fontawesome-svg-core"
 
-type MyLikeIconProps = {
-  className: string
-  handleClick: ((event: React.MouseEvent<SVGSVGElement, MouseEvent>) => void) | undefined
-  iconHearts: IconProp
-}
-
-export const LikeIcon:React.FC<MyLikeIconProps> = ({ className, handleClick, iconHearts }) => {
+export const LikeIcon = ({ className, handleClick, iconHearts }) => {
   return <FontAwesomeIcon className={ className } onClick={ handleClick } icon={ iconHearts }/>
 }
