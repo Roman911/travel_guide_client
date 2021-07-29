121828a49af2786866e39f4564bb8665c8d9f949
roman <roma-lysyk@ukr.net>
2021-07-29 20:03:19 +0300

    Fixed location information

--- pages/create-direction.tsx
+++ pages/create-direction.tsx
@@ -1,8 +1,17 @@
 import React from "react"
+import { useDispatch } from 'react-redux'
+import { locationsActions } from '../redux/actions'
 import {  MainLayout } from "../Components"
 import { CreateDirections } from "../modules"
 
 const CreateDirection: React.FC = (): any => {
+  const dispatch = useDispatch()
+  React.useEffect(() => {
+    dispatch(locationsActions.changeData({ mapContainerStyle: { height: "calc(100vh - 200px)", width: "100%" }} ))
+  }, [])
+
+  console.log('render CreateDirection')
+
   return <MainLayout title='Create Route' header='РЕДАГУВАННЯ' >
     <CreateDirections />
   </MainLayout>
